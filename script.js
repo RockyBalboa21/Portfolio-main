08898ca863354c02666b09ec3ce775750088de28
RockyBalboa21 <aliaxandr.gerasimau.@gmail.com>
2025-03-24 15:33:40 +0300

    добавил исчезновение для шапки по скроллу

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ let themeButton = document.querySelector('.theme-button');
 themeButton.onclick = function () {
     page.classList.toggle('dark-theme');
 };
+
+document.addEventListener("scroll", function () {
+    const header = document.querySelector(".page-header");
+    const scrollLimit = 600; // Укажите ограничение по скроллу (в пикселях)
+
+    if (window.scrollY > scrollLimit) {
+        header.style.position = "absolute"; // Перестает быть "sticky"
+        header.style.top = `${scrollLimit}px`;
+    } else {
+        header.style.position = "sticky"; // Возвращается к "sticky"
+        header.style.top = "0";
+    }
+});
